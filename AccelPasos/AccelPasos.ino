5d2d95a6dbe98ce738284231d659f4815a97435b
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-02-25 17:22:06 -0600

    añade propia implementacion de aceleracion en un motor a pasos

---
+++ AccelPasos/AccelPasos.ino
@@ -0,0 +1,25 @@
+#include <AccelStepper.h>
+const int pinStart = 0;
+const int pinMtr0 = 1;
+const int pinMtr1 = 2;
+const int pinMtr2 = 3;
+const int pinMtr3 = 4;
+uint32_t period;
+
+AccelStepper motor;
+void setup(){
+	motor = new AccelStepper(4, pinMtr0, pinMtr1, pinMtr2, pinMtr3);
+	motor.setMaxSpeed(1000);
+	motor.setAcceleration(motor.maxSpeed()/5);	
+}
+
+void loop(){
+	 //if(digitalRead(pinStart)){
+		period = 10 * 1000L;
+		for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
+  			motor.move(5);
+	   		motor.run();
+		}
+	//}
+}
+
--- AcelerarPasos/AcelerarPasos.ino
+++ AcelerarPasos/AcelerarPasos.ino
@@ -1,25 +1,28 @@
-#include <AccelStepper.h>
+#include <Stepper.h>
+
 const int pinStart = 0;
 const int pinMtr0 = 1;
 const int pinMtr1 = 2;
 const int pinMtr2 = 3;
 const int pinMtr3 = 4;
-uint32_t period;
+int vel, velMax;
+bool acelerar;
 
+Stepper stepper;
 
 void setup(){
-	AccelStepper motor = new AccelStepper(4, pinMtr0, pinMtr1, pinMtr2, pinMtr3);
-	motor.setMaxSpeed(1000);
-	motor.setAcceleration(motor.maxSpeed()/5)	
+	stepper = new Stepper(4, pinMtr0, pinMtr1, pinMtr2, pinMtr3);
+	vel = 1
 }
 
 void loop(){
 	if(digitalRead(pinStart)){
-		period = 10 * 1000L;
-		for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
-			motor.move(5);
-	   		motor.run();
+		for(int vel; vel < velMax; vel++){
+			if(!acelrar){
+
+			}else{
+				
+			}
 		}
 	}
 }
-
