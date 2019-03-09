1d50641da9dd5ff952bc4fc146d9d3796b3b48ac
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-03-09 13:05:07 -0600

    Modificaciones en la practica 1

--- AcelerarPasos/AcelerarPasos.ino
+++ AcelerarPasos/AcelerarPasos.ino
@@ -1,28 +1,47 @@
 #include <Stepper.h>
 
-const int pinStart = 0;
-const int pinMtr0 = 1;
-const int pinMtr1 = 2;
-const int pinMtr2 = 3;
-const int pinMtr3 = 4;
-int vel, velMax;
+const int pinStart = 2;
+const int pinMtr0 = 3;
+const int pinMtr1 = 4;
+const int pinMtr2 = 5;
+const int pinMtr3 = 6;
+int retardo;
 bool acelerar;
 
-Stepper stepper;
+Stepper stepper (2038, pinMtr0, pinMtr1, pinMtr2, pinMtr3);
+
 
 void setup(){
-	stepper = new Stepper(4, pinMtr0, pinMtr1, pinMtr2, pinMtr3);
-	vel = 1
+  Serial.begin(19200);
+  acelerar = false;
+  stepper.setSpeed(200);
+  pinMode(pinStart, INPUT);
+  stepper.setSpeed(6);
 }
 
 void loop(){
-	if(digitalRead(pinStart)){
-		for(int vel; vel < velMax; vel++){
-			if(!acelrar){
-
-			}else{
-				
+	if(digitalRead(pinStart)!=HIGH){
+    Serial.print("AHHHH");
+		acelerar = !acelerar;
+		if(acelerar){
+      Serial.print("accelera");
+			retardo=500;
+     		while (retardo>=100){
+         		stepper.step(1); 
+         		delay(retardo);
+            retardo = retardo - 1;
+        	} 
+         while(digitalRead(pinStart)!=HIGH){
+            stepper.step(1);
+            delay(retardo);
+         }
+		}else{
+			while(retardo<=500){
+				stepper.step(1);
+				delay(retardo);
+				retardo = retardo + 2;
 			}
 		}
+   delay(300);
 	}
 }
